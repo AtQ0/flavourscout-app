75d4bf1a0e1ed6eab27c0d689f66e561f690d978
AtQ0 <mail@atkobabic.com>
2024-08-23 14:20:01 +0200

    added a clear all button in SearchHeader.js

--- src/components/SearchHeader/SearchHeader.js
+++ src/components/SearchHeader/SearchHeader.js
@@ -1,7 +1,8 @@
 
 import React, { useState } from 'react';
-import { TextInput, StyleSheet, View } from 'react-native';
+import { TextInput, StyleSheet, View, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Svg, { Circle, Line } from 'react-native-svg';
 
 export default function SearchHeader() {
     const [searchTerm, setSearchTerm] = useState('');
@@ -13,6 +14,9 @@ export default function SearchHeader() {
         navigation.navigate('SearchScreen', { searchTerm: text }); // Pass searchTerm as parameter
     };
 
+    const clearSearch = () => {
+        setSearchTerm('');
+    };
     return (
         <View style={styles.headerContainer}>
             <TextInput
@@ -23,6 +27,15 @@ export default function SearchHeader() {
                 onChangeText={handleSearchChange} // Call handleSearchChange on text input change
                 value={searchTerm}
             />
+            {searchTerm ? (
+                <Pressable style={styles.clearButton} onPress={clearSearch}>
+                    <Svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <Circle cx="12" cy="12" r="10" stroke="#000000" strokeWidth="2" />
+                        <Line x1="15" y1="9" x2="9" y2="15" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <Line x1="9" y1="9" x2="15" y2="15" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                    </Svg>
+                </Pressable>
+            ) : null}
         </View>
     );
 }
@@ -31,6 +44,7 @@ const styles = StyleSheet.create({
     headerContainer: {
         flex: 1,
         justifyContent: 'center',
+        // backgroundColor: '#FFFFFF',
     },
     searchInput: {
         height: 35,
@@ -40,5 +54,9 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         width: 325,
         color: '#000',
+    },
+    clearButton: {
+        position: 'absolute',
+        right: -5,
     },
 });
